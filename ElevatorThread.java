624c7f3f408eb797fcc92a19d98ee897a5d47b1f
DavisRomn <davisabr@gmail.com>
2017-10-21 12:23:56 -0600

    sadf

--- ElevatorThread.java
+++ ElevatorThread.java
@@ -14,13 +14,13 @@ class ElevatorThread extends Thread
     {
         while(true){
             for (int i = 0; i < e.destinations.size(); i++){
-                if (e.destinations.get(i) > e.getHeight() / 3 && e.getDirection() == 1){
+                } if (e.destinations.get(i) > e.getHeight() / 3 && e.getDirection() == 1){
                     e.moveUp(e.destinations.get(i), i);
                 } else if(e.destinations.get(i) < e.getHeight() / 3 && e.getDirection() == -1){
                     e.moveDown(e.destinations.get(i), i);
-                } else if(e.destinations.get(i) > e.getHeight() / 3 && e.getDirection() == 0){
+                } else if(e.destinations.get(i) > e.getHeight() / 3 && e.getDirection() == 0 && e.destinations.get(i) != e.getHeight() / 3){
                     e.moveUp(e.destinations.get(i), i);
-                } else if(e.destinations.get(i) < e.getHeight() / 3 && e.getDirection() == 0){
+                } else if(e.destinations.get(i) < e.getHeight() / 3 && e.getDirection() == 0 && e.destinations.get(i) != e.getHeight() / 3){
                     e.moveDown(e.destinations.get(i), i);
                 }
             }
