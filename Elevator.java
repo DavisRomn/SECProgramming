81e2a6a63a9c6f2179dc86ecd0f5ece653b4be25
DavisRomn <davisabr@gmail.com>
2017-10-21 09:09:46 -0600

    Added Elevator and Scheduling Class ... Finished beta version of Elevator

---
+++ Elevator.java
@@ -0,0 +1,94 @@
+import java.util.Vector<E>;
+
+class Elevator
+{
+
+    // Speed of the Elevator
+    public volatile double speed;
+
+    //Height in-comparison to the building with the bottom of the elevator
+    public double height;
+
+    //Boolean for if already occupado
+    public boolean occupied;
+
+    // Int value of direction: -1 is down, 0 is stopped and 1 is up
+    public volatile int direction;
+
+    //Acceleration of the Elevators
+    private double acceleration = 0.5;
+
+    //Max speed
+    private double MAX_SPEED = 3;
+
+    // Destinations of Elevator in m
+    private Vector<int> destinations;
+
+    //Default Constructor
+    Elevator()
+    {
+        occupied = false;
+        speed = 0;
+        height = 0;
+        direction = 0;
+    }
+
+    // Returns the speed of the Elevator
+    public double getSpeed()
+    {
+        return speed;
+    }
+
+    // Returns the height (Location) of the elevator
+    public double getHeight()
+    {
+        return height;
+    }
+
+    //Get direction value
+    public int getDirection()
+    {
+        return direction;
+    }
+
+    public Vector<int> getDestinations()
+    {
+        return destinations;
+    }
+
+    public callUp(int floor)
+    {
+        direction = 1;
+        destinations.add(floor * 3);
+        /////////////////////////////////////////////////////////////////////////////////////// FIX TIMING
+        while (height < newDest * 3)
+        {
+            speed + 0.5;
+            height + speed;
+        }
+        direction = 0;
+        destinations.remove(floor * 3);
+    }
+    public callDown(int floor)
+    {
+        destinations.add(floor * 3);
+        direction = -1;
+        /////////////////////////////////////////////////////////////////////////////////////// FIX TIMING
+        while (height > newDest * 3)
+        {
+            speed + 0.5;
+            height - speed;
+        }
+        direction = 0;
+        destinations.remove(floor * 3);
+    }
+
+    public boolean getStopped()
+    {
+        if (direction = 0){
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
---
+++ Scheduling.java
@@ -0,0 +1,11 @@
+import java.util.Vector<E>;
+
+class Scheduling
+{
+    private Vector<Elevator> Elevators;
+
+    Scheduling()
+    {
+        return;
+    }
+}
