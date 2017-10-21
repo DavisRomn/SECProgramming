2130d6ddaf1274b5c4121c076184e33cfeef91ea
DavisRomn <davisabr@gmail.com>
2017-10-21 10:38:38 -0600

    hedsagaetga

--- Elevator.java
+++ Elevator.java
@@ -8,22 +8,22 @@ class Elevator extends Thread
     public volatile double speed;
 
     //Height in-comparison to the building with the bottom of the elevator
-    public double height;
+    public volatile double height;
 
     //Boolean for if already occupado
-    public boolean occupied;
+    public volatile boolean occupied;
 
     // Int value of direction: -1 is down, 0 is stopped and 1 is up
     public volatile int direction;
 
     //Acceleration of the Elevators
-    private double acceleration = 0.5;
+    private volatile double acceleration = 0.5;
 
     //Max speed
     private double MAX_SPEED = 3;
 
     // Destinations of Elevator in m
-    private Vector<Integer> destinations;
+    private volatile Vector<Integer> destinations;
 
     private Thread movement;
 
@@ -40,8 +40,8 @@ class Elevator extends Thread
 
     public void run()
     {
-        while(1){
-            
+        while(true){
+
         }
     }
 
@@ -70,29 +70,39 @@ class Elevator extends Thread
 
     public void moveUp(int floor)
     {
-        direction = 1;
-        destinations.add(floor * 3);
-        /////////////////////////////////////////////////////////////////////////////////////// FIX TIMING
-        while (height < floor * 3)
+        try{
+            direction = 1;
+            destinations.add(floor * 3);
+            while (height < floor * 3)
+            {
+                speed += 0.5;
+                height += speed;
+                movement.sleep(1000);
+            }
+            direction = 0;
+            destinations.remove(floor * 3);
+        } catch(InterruptedException e)
         {
-            speed += 0.5;
-            height += speed;
+            e.printStackTrace();
         }
-        direction = 0;
-        destinations.remove(floor * 3);
     }
     public void moveDown(int floor)
     {
-        destinations.add(floor * 3);
-        direction = -1;
-        /////////////////////////////////////////////////////////////////////////////////////// FIX TIMING
-        while (height > floor * 3)
+        try{
+            destinations.add(floor * 3);
+            direction = -1;
+            while (height > floor * 3)
+            {
+                speed += 0.5;
+                height -= speed;
+                movement.sleep(1000);
+            }
+            direction = 0;
+            destinations.remove(floor * 3);
+        } catch(InterruptedException e)
         {
-            speed += 0.5;
-            height -= speed;
+            e.printStackTrace();
         }
-        direction = 0;
-        destinations.remove(floor * 3);
     }
 
     public boolean getStopped()
